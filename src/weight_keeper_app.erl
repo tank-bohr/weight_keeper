08b03c9c720bb49b1cccace9a933f308eabbe32c
Alexey Nikitin <tank@bohr.su>
2022-04-02 21:29:34 +0400

    Supress log progress reports

--- src/weight_keeper_app.erl
+++ src/weight_keeper_app.erl
@@ -5,7 +5,11 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    ok = logger:set_primary_config(filters, log_filters()),
     weight_keeper_sup:start_link().
 
 stop(_State) ->
     ok.
+
+log_filters() ->
+    [{suppress_progress, {fun logger_filters:progress/2, stop}}].
